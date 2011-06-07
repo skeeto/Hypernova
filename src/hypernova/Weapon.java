68e31897880b52cbdebb8cda18fe526ad9f5cec8
Christopher Wellons <mosquitopsu@gmail.com>
2011-06-07 19:53:13 -0400

    Actually read in the sound effect info.

--- src/hypernova/Weapon.java
+++ src/hypernova/Weapon.java
@@ -8,12 +8,16 @@ public class Weapon {
     public static final double DEFAULT_COOLDOWN = 3.0;
     public static final double DEFAULT_MASS = 1.0;
     public static final String DEFAULT_AMMO = "bolt";
+    public static final int DEFAULT_CHANNEL = 0;
+    public static final int DEFAULT_NOTE = 40;
+    public static final int DEFAULT_VOLUME = 75;
 
     public final String name, info;
 
     private double cooldown, timeout;
     private double mass;
     private Ammo ammo;
+    private int channel, note, volume;
 
     private static Logger log = Logger.getLogger("Weapon");
 
@@ -39,6 +43,10 @@ public class Weapon {
                                    props.getProperty("info"));
         weapon.cooldown = attempt(props, "cooldown", DEFAULT_COOLDOWN);
         weapon.mass = attempt(props, "mass", DEFAULT_MASS);
+        weapon.channel = (int) Weapon.attempt(props, "channel",
+                                              DEFAULT_CHANNEL);
+        weapon.note = (int) Weapon.attempt(props, "note", DEFAULT_NOTE);
+        weapon.volume = (int) Weapon.attempt(props, "volume", DEFAULT_VOLUME);
         String ammoname = props.getProperty("ammo");
         if (ammoname == null)
             ammoname = DEFAULT_AMMO;
@@ -60,7 +68,7 @@ public class Weapon {
 
     public void fire(Mass src) {
         if (timeout <= 0) {
-            Sound.play(0, 40, 75);
+            Sound.play(channel, note, volume);
             Hypernova.universe.add(ammo.copy(src));
             timeout = cooldown;
         }
