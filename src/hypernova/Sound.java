4f4fdf637c2ac37d5cf91f5c7e83131f9d7e193d
Christopher Wellons <mosquitopsu@gmail.com>
2011-06-10 17:37:06 -0400

    Gut the sound system since MIDI isn't very useful.
    Too many systems don't have MIDI support, so there's no reason to use
    it here.

--- src/hypernova/Sound.java
+++ src/hypernova/Sound.java
@@ -1,36 +1,17 @@
 package hypernova;
 
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.Synthesizer;
-import javax.sound.midi.MidiChannel;
-
 import org.apache.log4j.Logger;
 
 public class Sound {
-
     public static boolean enabled;
-    private static Synthesizer synth;
-    private static MidiChannel[] channels;
 
     private static Logger log = Logger.getLogger("Sound");
 
     public static void init() {
-        try {
-            log.info("Setting up MIDI");
-            synth = MidiSystem.getSynthesizer();
-            synth.open();
-            enabled = true;
-        } catch (Exception e) {
-            log.warn("MIDI unavailable, sound disabled: " + e.getMessage());
-            return;
-        }
-        channels = synth.getChannels();
-        channels[0].programChange(32);
-        log.info("MIDI setup complete");
+        log.info("Setting up sound");
+        enabled = true;
     }
 
-    public static void play(int channel, int note, int velocity) {
-        if (enabled)
-            channels[channel].noteOn(note, velocity);
+    public static void play(String name) {
     }
 }
