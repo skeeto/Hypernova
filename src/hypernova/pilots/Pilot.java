703beef7af002b5dc7dcf0ead99f71ae4dcd613b
Christopher Wellons <mosquitopsu@gmail.com>
2011-06-10 15:52:51 -0400

    Add some Javadoc to Pilot.

--- src/hypernova/pilots/Pilot.java
+++ src/hypernova/pilots/Pilot.java
@@ -2,12 +2,28 @@ package hypernova.pilots;
 
 import hypernova.Ship;
 
+/**
+ * This class is the brains behind a ship. It controls the ships
+ * engines and weapons in order to meet some goal, such as attacking
+ * enemy players, collecting cargo, or just following a patrol route.
+ *
+ * A number of methods are (will be) provided for higher-level control
+ * of ship movement, because low level engine control is difficult to
+ * use effictively.
+ */
 public abstract class Pilot {
+    /** The ship being controlled by this pilot. */
     protected final Ship ship;
 
     public Pilot(Ship ship) {
         this.ship = ship;
     }
 
+    /**
+     * Called each step of iteration of the universe just before the
+     * driven ship's state is updated. In this method, the ship should
+     * be manipulated through the various driving commands that
+     * control the level of engines and weapon firing.
+     */
     public abstract void drive();
 }
