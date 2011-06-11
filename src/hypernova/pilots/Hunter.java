3e156eb08e26f7fd47c85a87896805edfc5db5bd
Christopher Wellons <mosquitopsu@gmail.com>
2011-06-10 23:52:33 -0400

    Generalize the hunter-style pilot.

---
+++ src/hypernova/pilots/Hunter.java
@@ -0,0 +1,37 @@
+package hypernova.pilots;
+
+import hypernova.Ship;
+
+public class Hunter extends Pilot {
+    public static final double TARGET_DIST = 60.0;
+
+    protected Ship target;
+
+    public Hunter(Ship ship, Ship target) {
+        super(ship);
+        setTarget(target);
+    }
+
+    public void setTarget(Ship target) {
+        this.target = target;
+    }
+
+    @Override
+    public void drive(double dt) {
+        if (target == null) return;
+        double x = ship.getX(0);
+        double y = ship.getY(0);
+        double px = target.getX(0);
+        double py = target.getY(0);
+        double dir = Math.atan2(py - y, px - x);
+        face(dt, dir);
+
+        double diff = dir - ship.getA(0);
+        if (Math.abs(diff) < 0.05 && target.isActive()) {
+            ship.fire(0);
+        }
+
+        double dist = Math.sqrt((px - x) * (px - x) + (py - y) * (py - y));
+        ship.setEngines((dist - TARGET_DIST) / 250);
+    }
+}
--- src/hypernova/pilots/PlayerHunter.java
+++ src/hypernova/pilots/PlayerHunter.java
@@ -2,31 +2,17 @@ package hypernova.pilots;
 
 import hypernova.Ship;
 
-public class PlayerHunter extends Pilot {
-    public static final double TARGET_DIST = 60.0;
-
-    private Ship player;
-
+public class PlayerHunter extends Hunter {
     public PlayerHunter(Ship ship) {
-        super(ship);
+        super(ship, null);
     }
 
-    @Override
+    private Ship getPlayer() {
+        return hypernova.Hypernova.universe.getPlayer();
+    }
+
     public void drive(double dt) {
-        player = hypernova.Hypernova.universe.getPlayer();
-        double x = ship.getX(0);
-        double y = ship.getY(0);
-        double px = player.getX(0);
-        double py = player.getY(0);
-        double dir = Math.atan2(py - y, px - x);
-        face(dt, dir);
-
-        double diff = dir - ship.getA(0);
-        if (Math.abs(diff) < 0.05 && player.isActive()) {
-            ship.fire(0);
-        }
-
-        double dist = Math.sqrt((px - x) * (px - x) + (py - y) * (py - y));
-        ship.setEngines((dist - TARGET_DIST) / 250);
+        if (target == null || !target.isActive()) target = getPlayer();
+        super.drive(dt);
     }
 }
