a124fdd749aad35d4d727afa62b6819172e04eff
Christopher Wellons <mosquitopsu@gmail.com>
2011-06-11 16:47:44 -0400

    New HunterSeeker bot, seeks out new enemies when target dies.

---
+++ src/hypernova/pilots/HunterSeeker.java
@@ -0,0 +1,44 @@
+package hypernova.pilots;
+
+import hypernova.Mass;
+import hypernova.Ship;
+import hypernova.Faction;
+import hypernova.Universe;
+
+public class HunterSeeker extends Hunter {
+    public HunterSeeker(Ship ship) {
+        super(ship, null);
+    }
+
+    @Override
+    public void drive(double dt) {
+        if (target == null || !target.isActive()) {
+            Mass old = target;
+            target = null;
+            Faction faction = ship.getFaction();
+            double tdist = 0;
+            for (Mass m : Universe.get().getShips()) {
+                if (m.getFaction() == faction) continue;
+                if (target == null) {
+                    target = m;
+                    tdist = distance2(m);
+                    continue;
+                }
+                double dist = distance2(m);
+                if (dist < tdist) {
+                    target = m;
+                    tdist = dist;
+                }
+            }
+            if (target == null)
+                target = old;
+        }
+        super.drive(dt);
+    }
+
+    private double distance2(Mass m) {
+        double x = m.getX(0) - ship.getX(0);
+        double y = m.getY(0) - ship.getY(0);
+        return x * x + y * y;
+    }
+}
