672794ac2041c7934423528dd6373c3e4c97cbac
Brian Taylor <el.wubo@gmail.com>
2011-06-11 00:46:26 -0400

    updated scenario with new features

--- src/hypernova/activities/test.groovy
+++ src/hypernova/activities/test.groovy
@@ -11,7 +11,8 @@ withNewPlayer("tenderfoot") { player ->
 
 withNewShip("tenderfoot") { dummy ->
     dummy.setPosition(45, 105, Math.PI / 3).setFaction("Humans");
-    dummy.setWeapon("blaster", 0);
+    dummy.setWeapon("blaster", 0).setEngine("tourist", 0);
+    dummy.setPilot(new hypernova.pilots.CirclePilot(dummy, 1.0))
     dummy.setSize(6.0);
 }
 
@@ -21,3 +22,11 @@ withNewMass("small-station") { station ->
     station.setSize(30.0);
 }
 
+(0..3).each() { idx ->
+    withNewShip("tenderfoot") { invader ->
+       invader.setWeapon("blaster", 0).setEngine("tourist", 0)
+       invader.setSize(3.5).setFaction("Invaders")
+       invader.setPosition(200.0 * idx, 200.0 + idx * 10.0, 0.0)
+       invader.setPilot(new hypernova.pilots.PlayerHunter(invader))
+    }
+}
