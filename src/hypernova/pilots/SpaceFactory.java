a8a241d9e6cdcc549d5d5b3f8af0975597d4e17e
Christopher Wellons <mosquitopsu@gmail.com>
2011-06-12 22:33:46 -0400

    Add gold drops to the SpaceFactory.

--- src/hypernova/pilots/SpaceFactory.java
+++ src/hypernova/pilots/SpaceFactory.java
@@ -1,19 +1,31 @@
 package hypernova.pilots;
 
+import java.util.Random;
+
 import hypernova.Ship;
 import hypernova.Universe;
 
 public class SpaceFactory extends Pilot {
+    private final Random RNG = new Random();
     private String tooling;
     private PilotFactory pilots;
     private double speed, timeout;
+    private double meanGold;
+    private double varGold;
+    private double dropRate;
 
-    public SpaceFactory(Ship ship, String tooling, PilotFactory pilots,
-                        double speed) {
+    public SpaceFactory(Ship ship, String tooling, double speed,
+                        PilotFactory pilots) {
         super(ship);
         this.tooling = tooling;
         this.pilots = pilots;
         this.speed = this.timeout = speed;
+    }
+
+    public void addGold(double mean, double var, double rate) {
+        meanGold = mean;
+        varGold = var;
+        dropRate = rate;
     }
 
     @Override
@@ -27,6 +39,10 @@ public class SpaceFactory extends Pilot {
         forged.setFaction(ship.getFaction());
         forged.setPilot(pilots.create(forged));
         forged.setPosition(ship);
+        if (RNG.nextDouble() < dropRate) {
+            long gold = (long) (RNG.nextGaussian() * varGold + meanGold);
+            if (gold > 0) forged.store(gold);
+        }
         Universe.get().add(forged);
     }
 }
