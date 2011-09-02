5e9802b1d250af569e9d582d6fe54e2aea32937e
plurSKI <black.gavin@gmail.com>
2011-09-02 19:05:30 -0400

    Quick hack for async sound

---
+++ src/hypernova/MinimThread.java
@@ -0,0 +1,21 @@
+package hypernova;
+
+import processing.core.PApplet;
+import ddf.minim.*;
+import ddf.minim.analysis.*;
+
+
+/**
+ * An audio wrapper for minim with the following features
+ *  - Provides real-time FFT averages
+ *  - Performs crossfading between successive tracks
+ *  - Plays sound effects
+ * @author  plurSKI
+ */
+public class MinimThread extends Thread
+{
+         private String song = "";
+         MinimThread (String file) {this.song = file;}
+         public void run() { MinimWrapper.playSound(song);}
+
+}
