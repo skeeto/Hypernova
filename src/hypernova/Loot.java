9d9f247cf5479dbf76b71c33da8efdc58c2032aa
plurSKI <black.gavin@gmail.com>
2011-09-05 11:17:01 -0400

    Magnetic Money

--- src/hypernova/Loot.java
+++ src/hypernova/Loot.java
@@ -2,12 +2,16 @@ package hypernova;
 
 import java.awt.geom.Rectangle2D;
 import java.util.Random;
+import java.lang.Math;
 
 public class Loot extends Mass {
+    public static final double MAG_MIN = 15;
+    public static final double MAG_RATE = 8;
     public static final int LOOT_TTL = 5000;
     public static final double SPIN_RATE = 0.1;
     public static final double DRIFT_RATE = 0.5;
     private static final Random RNG = new Random();
+    private static double magnetRange = 50;
 
     private Mass cargo;
     private long gold;
@@ -29,6 +33,8 @@ public class Loot extends Mass {
         this.gold = gold;
     }
 
+    public static void setLootMagnetRange(double range) { magnetRange = range; }
+
     @Override
     public void step(double t) {
         x[2] = 0;
@@ -36,7 +42,26 @@ public class Loot extends Mass {
         super.step(t);
         Rectangle2D bounds = getHit().getBounds2D();
         Ship player = Universe.get().getPlayer();
-        if (player.getHit().intersects(bounds)) {
+          
+        // Magnetic money
+        Rectangle2D magnet = new Rectangle2D.Double( x[0] - magnetRange, y[0] - magnetRange
+                                                   , magnetRange * 2, magnetRange * 2);
+        double px = player.getX(0);
+        double py = player.getY(0); 
+        short doCashout = 0;//false;
+        if(magnet.contains(px,py))
+        {
+           double dx = px - x[0];
+           double dy = py - y[0];
+           if( dx > MAG_MIN || dx < -MAG_MIN ) x[0] += dx/MAG_RATE;
+           else doCashout ++;
+           if( dy > MAG_MIN || dy < -MAG_MIN ) y[0] += dy/MAG_RATE;
+           else doCashout ++;
+        }
+
+        if ( doCashout >= 2 ) { //player.getHit().intersects(bounds)) {
+            x[1] = player.getX(1);
+            y[1] = player.getY(1);
             cashout(player);
         } 
             
