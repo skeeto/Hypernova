54efeff8a91cd0adb141ca6bea2420843454d0df
plurSKI <black.gavin@gmail.com>
2012-01-21 11:53:27 -0500

    Made ChopEffect slighltly less irritating

--- src/hypernova/sounds/ChopEffect.java
+++ src/hypernova/sounds/ChopEffect.java
@@ -6,7 +6,8 @@ public class ChopEffect implements AudioEffect
 {
   public void process(float[] samp)
   {
-    for (int i = 0; i < samp.length; i++) samp[i] = samp[i % 200];
+    System.out.println(samp.length);
+    for (int i = 0; i < samp.length; i++) samp[i] = samp[i % (samp.length / 3)];
   }
   
   public void process(float[] left, float[] right)
