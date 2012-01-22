d07514aebc29fab397096df5a5982faddf4d6f5a
plurSKI <black.gavin@gmail.com>
2012-01-22 13:59:06 -0500

    Fixed VolumeEffect coefficients

--- src/hypernova/sounds/VolumeEffect.java
+++ src/hypernova/sounds/VolumeEffect.java
@@ -7,7 +7,7 @@ public class VolumeEffect implements AudioEffect
   public static int amount = 0;
   public void process(float[] samp)
   {
-    for (int i = 0; i < samp.length; i++) samp[i] = (samp[i]*amount) / 500 ;
+    for (int i = 0; i < samp.length; i++) samp[i] = (samp[i]*(amount + 100)) / 1000 ;
   }
   
   public void process(float[] left, float[] right)
