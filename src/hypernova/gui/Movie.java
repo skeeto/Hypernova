d8ae82b9a3aa3c4195504e3a7827159634f908f9
plurSKI <black.gavin@gmail.com>
2012-01-22 19:46:58 -0500

    Ability to play 'movies'

---
+++ src/hypernova/gui/Movie.java
@@ -0,0 +1,39 @@
+package hypernova.gui;
+
+import java.awt.event.KeyEvent;
+
+import hypernova.Universe;
+import hypernova.Hypernova;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+
+public class Movie
+{
+    private static String movie = "";
+    private static int frame = 0;
+    private static int w = 0;
+    private static int h = 0;
+
+    public static void render(Graphics2D g2d) {
+      File f;
+      BufferedImage img = null;
+      try {
+        f = new File("movies/" 
+                    + movie 
+                    + "/" + "/image" 
+                    + (frame++) + ".png"
+                    );
+       img = ImageIO.read(f);
+     } catch (Exception e) {}
+     g2d.drawImage(img, 0, 0, w, h, null);
+    }
+
+    public static void begin(String m) {
+      movie = m;
+      w = Hypernova.getViewer().getWidth();
+      h = Hypernova.getViewer().getHeight();
+      frame = 0;
+    }
+}
