6ac17eea41674cfb8f73cf8b7fc9f2480baef6af
plurSKI <black.gavin@gmail.com>
2012-01-29 07:12:13 -0500

    Pause takes a screenshot

--- src/hypernova/gui/menus/Pause.java
+++ src/hypernova/gui/menus/Pause.java
@@ -2,10 +2,36 @@ package hypernova.gui.menus;
 
 import hypernova.gui.MenuScreen;
 import hypernova.gui.Menu;
+import hypernova.gui.Viewer;
 import hypernova.Universe;
+import hypernova.Hypernova;
+import java.io.File;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.imageio.ImageIO;
+import org.apache.log4j.Logger;
 
 public class Pause extends MenuScreen
 {
+  private static Logger log = Logger.getLogger("gui.menus.Pause");
+  private BufferedImage img;
+
+  public Pause() {
+    // Screenshot
+    Viewer v = Hypernova.getViewer();
+    img = new BufferedImage(v.getWidth(), v.getHeight(),
+                            BufferedImage.TYPE_INT_ARGB);
+    Graphics gImg = img.getGraphics();
+    v.paintComponent(gImg);
+    try { ImageIO.write(img, "PNG", new File("test.png"));}
+    catch (Exception e) { log.error("Pause screenshot failed"); }
+
+    gImg.dispose();
+  }
+
   public void back() {
     Universe.get().togglePause(false);
     Menu.setInMenu(false);
